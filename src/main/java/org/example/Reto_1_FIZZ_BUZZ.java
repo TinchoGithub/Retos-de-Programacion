31cfca502cf0d664c65f8128e28f77bf150536a8
Martín Perez <100448340+TinchoGithub@users.noreply.github.com>
2024-05-12 00:54:54 -0300

    Resuelvo 1er Reto ´FIZZ BUZZ´

---
+++ src/main/java/org/example/Reto_1_FIZZ_BUZZ.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Reto_1_FIZZ_BUZZ {
+    public static void main(String[] args){
+
+
+        for (int i=1; i<=100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FIZZBUZZ");
+            } else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            } else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
